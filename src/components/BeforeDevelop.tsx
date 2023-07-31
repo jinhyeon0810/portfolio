4ad0324c11bd22ef4e7dae0aecb14893ceae1c83
jinhyeon0810 <jinhyeon0810@gmail.com>
2023-07-31 23:33:38 +0900

    포트폴리오 수정

--- src/components/BeforeDevelop.tsx
+++ src/components/BeforeDevelop.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import ImageSrc from "../assets/개발이전이력1.png";
-import ImageTwoSrc from "../assets/개발이전이력2.png";
+import ImageSrc from "../../public/assets/개발이전이력1.png";
+import ImageTwoSrc from "../../public/assets/개발이전이력2.png";
 
 export default function BeforeDevelop() {
   return (
