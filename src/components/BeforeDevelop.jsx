8c92fa02ba582c7d9bb2805e68e9dbbdae83e064
unknown <jinhyeon0810@gmail.com>
2023-08-04 20:27:15 +0900

    포폴 data폴더 생성 및 반응형 수정

--- src/components/BeforeDevelop.jsx
+++ src/components/BeforeDevelop.jsx
@@ -17,9 +17,16 @@ const ImageSection = styled.div`
   align-items: center;
   gap: 50px;
   margin-bottom: 50px;
+  @media (max-width: 1289px) {
+    flex-direction: column;
+  }
 `;
 
 const Image = styled.img`
   width: 600px;
   height: 400px;
+  @media (max-width: 646px) {
+    width: 300px;
+    height: 100%;
+  }
 `;
---
+++ src/data/data.jsx
@@ -0,0 +1,83 @@
+import StudyImageSrc from "../../src/assets/thumnails/youtube썸네일.png";
+import SecondStudyImageSrc from "../../src/assets/thumnails/htmlcss1.png";
+import ThirdStudyImageSrc from "../../src/assets/thumnails/mbtireact.png";
+import FourthStudyImageSrc from "../../src/assets/thumnails/coupang-NavFooter.png";
+import CoupangCategoryImageSrc from "../../src/assets/thumnails/coupang-category.png";
+import TodoListImageSrc from "../../src/assets/thumnails/todolist.png";
+import StarRateImageSrc from "../../src/assets/thumnails/starRateThumnail.png";
+import DatePickerImageSrc from "../../src/assets/thumnails/datepickerThumnail.png";
+
+export const datas = [
+  {
+    src: SecondStudyImageSrc,
+    text: "Coupang-CloneCoding",
+    name: "Coupang 오늘의발견",
+    desc: "쿠팡 웹페이지 마크업 게시물 입니다.",
+    work: "HTML, CSS",
+    code: "https://github.com/jinhyeon0810/FirstTimeGit/tree/master/%EC%A0%9C%EB%A1%9C%EB%B2%A0%EC%9D%B4%EC%8A%A4%20%EA%B3%BC%EC%A0%9C/Mission-01/Mission-01",
+    web: "https://htmlstudy.vercel.app/",
+  },
+  {
+    src: CoupangCategoryImageSrc,
+    text: "Coupang-Category",
+    name: "Coupang 추천상품",
+    desc: "쿠팡 웹페이지 추천상품 영역 마크업 게시물입니다.",
+    work: "HTML, CSS",
+    code: "https://github.com/jinhyeon0810/FirstTimeGit/tree/master/%EC%A0%9C%EB%A1%9C%EB%B2%A0%EC%9D%B4%EC%8A%A4%20%EA%B3%BC%EC%A0%9C/Mission-03/Mission-03",
+    web: "https://coupang-category.vercel.app//",
+  },
+  {
+    src: FourthStudyImageSrc,
+    text: "Coupang-Nav/Footer",
+    name: "Coupang 헤더영역",
+    desc: "쿠팡 웹페이지 헤더영역 마크업 게시물입니다.",
+    work: "HTML, CSS",
+    code: "https://github.com/jinhyeon0810/FirstTimeGit/tree/master/%EC%A0%9C%EB%A1%9C%EB%B2%A0%EC%9D%B4%EC%8A%A4%20%EA%B3%BC%EC%A0%9C/Mission-05/Mission-05",
+    web: "https://coupang-navfooter.vercel.app/",
+  },
+  {
+    src: StarRateImageSrc,
+    text: "JavaScript-StarRate",
+    name: "Star-rate",
+    desc: "자바스크립트로 구현한 Star-rate입니다.",
+    work: "HTML, CSS, JavaScript",
+    code: "https://github.com/jinhyeon0810/JavaScript-Study/tree/main/template3",
+    web: "https://javascript-study-star-rate.vercel.app/",
+  },
+  {
+    src: DatePickerImageSrc,
+    text: "JavaScript-DatePicker",
+    name: "DatePicker",
+    desc: "자바스크립트로 구현한 DatePicker입니다.",
+    work: "HTML, CSS, JavaScript",
+    code: "https://github.com/jinhyeon0810/JavaScript-Study/tree/main/template4",
+    web: "https://javascript-study-datepicker.vercel.app/",
+  },
+  {
+    src: TodoListImageSrc,
+    text: "TodoList",
+    name: "TodoList 클론코딩",
+    desc: "오늘 할일을 기록할 수 있는 사이트입니다.",
+    work: "React, CSS",
+    code: "https://github.com/jinhyeon0810/clone-coding",
+    web: "https://clone-coding-todolist.vercel.app/",
+  },
+  {
+    src: StudyImageSrc,
+    text: "Youtube",
+    name: "Youtube 클론코딩",
+    desc: "유튜브 Open API를 활용한 사이트입니다.",
+    work: "React, TailwindCSS",
+    code: "https://github.com/jinhyeon0810/youtube-clone-coding",
+    web: "https://youtube-clone-coding-three.vercel.app/",
+  },
+  {
+    src: ThirdStudyImageSrc,
+    text: "MBTI",
+    name: "MBTI 클론코딩",
+    desc: "MBTI 궁합 확인할 수 있는 사이트입니다.",
+    work: "React, TypeScript, Styled-component",
+    code: "https://github.com/jinhyeon0810/mbtitest_ts/tree/main/practice/mbtitest_ts",
+    web: "https://mbtitest-clonecoding.vercel.app/",
+  },
+];
